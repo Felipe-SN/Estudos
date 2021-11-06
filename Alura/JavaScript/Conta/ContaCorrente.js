a5848b4b77005e116285ec258a3ee6c78569ad5a
Felipe_SN <felipewfk@gmail.com>
2021-11-06 13:49:54 -0300

    Sintax fix

--- Alura/JavaScript/Conta/ContaCorrente.js
+++ Alura/JavaScript/Conta/ContaCorrente.js
@@ -5,9 +5,9 @@ export class ContaCorrente extends Conta {
     constructor(cliente, agencia) {
         super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
-    };
+    }
     sacar(valor) {
         let taxa = 1.1;
         return this._sacar(valor, taxa);
-    };
-};
+    }
+}
--- Alura/JavaScript/Conta/ContaSalario.js
+++ Alura/JavaScript/Conta/ContaSalario.js
@@ -3,9 +3,9 @@ import { Conta } from "./Conta.js";
 export class ContaSalario extends Conta {
     constructor(cliente) {
         super(0, cliente, 1001)
-    };
+    }
     sacar(valor) {
         let taxa = 1.01;
         return this._sacar(valor, taxa);
-    };
-};
+    }
+}
