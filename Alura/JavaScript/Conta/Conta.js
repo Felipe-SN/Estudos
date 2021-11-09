7dfcd5085089f8617b318172b89b4804eb98c250
Felipe_SN <felipewfk@gmail.com>
2021-11-09 11:13:04 -0300

    fix identação

--- Alura/JavaScript/Conta/Conta.js
+++ Alura/JavaScript/Conta/Conta.js
@@ -1,39 +1,43 @@
 export class Conta {
-    constructor(saldoIni, cliente, agencia) {
-        if (this.constructor == Conta) {
-            throw new Error(`Não é possivel efetuar esta operação\nO objeto "conta" não pode ser instanciado diretamente.\nEsta é uma classe abstrata!`);
-        } else {
-            this._saldo = saldoIni;
-            this._cliente = cliente;
-            this._agencia = agencia;
-        }
-    }
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-    get cliente() {
-        return this._cliente;
-    }
-    sacar(valor) {
-        throw new Error(`O método sacar é abstrato!\nPor favor, sobrescreva este método!`);
-        // let taxa = 1;
-        // return this._sacar(valor, taxa);
-    }
-    _sacar(valor, taxa) {
-        const valorSacado = taxa * valor;
-        if (this._saldo >= valorSacado) {
-            this._saldo -= valorSacado;
-            return valorSacado;
-        }
-        return 0;
-    }
-    depositar(valor) {
-        this._saldo += valor;
-    }
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado)
-    }
+	constructor(saldoIni, cliente, agencia) {
+		if (this.constructor == Conta) {
+			throw new Error(
+				`Não é possivel efetuar esta operação\nO objeto "conta" não pode ser instanciado diretamente.\nEsta é uma classe abstrata!`
+			);
+		} else {
+			this._saldo = saldoIni;
+			this._cliente = cliente;
+			this._agencia = agencia;
+		}
+	}
+	set cliente(novoValor) {
+		if (novoValor instanceof Cliente) {
+			this._cliente = novoValor;
+		}
+	}
+	get cliente() {
+		return this._cliente;
+	}
+	sacar(valor) {
+		throw new Error(
+			`O método sacar é abstrato!\nPor favor, sobrescreva este método!`
+		);
+		// let taxa = 1;
+		// return this._sacar(valor, taxa);
+	}
+	_sacar(valor, taxa) {
+		const valorSacado = taxa * valor;
+		if (this._saldo >= valorSacado) {
+			this._saldo -= valorSacado;
+			return valorSacado;
+		}
+		return 0;
+	}
+	depositar(valor) {
+		this._saldo += valor;
+	}
+	transferir(valor, conta) {
+		const valorSacado = this.sacar(valor);
+		conta.depositar(valorSacado);
+	}
 }
--- Alura/JavaScript/Conta/ContaPoupanca.js
+++ Alura/JavaScript/Conta/ContaPoupanca.js
@@ -1,11 +1,11 @@
 import { Conta } from "./Conta.js";
 
 export class ContaPoupanca extends Conta {
-    constructor(saldoIni, cliente, agencia) {
-        super(saldoIni, cliente, agencia)
-    }
-    sacar(valor) {
-        let taxa = 1.02;
-        return this._sacar(valor, taxa);
-    }
+	constructor(saldoIni, cliente, agencia) {
+		super(saldoIni, cliente, agencia);
+	}
+	sacar(valor) {
+		let taxa = 1.02;
+		return this._sacar(valor, taxa);
+	}
 }
