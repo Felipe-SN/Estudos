83831a98435f7b48fc762f989580c2dd91505424
Felipe_SN <felipewfk@gmail.com>
2021-11-25 16:18:57 -0300

    aula 4.3

--- Alura/JS na Web Manipule o DOM com JS/projeto_inicial/assets/js/listaDeTarefas.js
+++ Alura/JS na Web Manipule o DOM com JS/projeto_inicial/assets/js/listaDeTarefas.js
@@ -1,12 +1,35 @@
 const criaTarefa = (evento) => {
 	evento.preventDefault();
 
-	const input = document.querySelector("[data-form-input]");
+	const lista = document.querySelector('[data-list]');
+	const input = document.querySelector('[data-form-input]');
 	const valor = input.value;
-	console.log(valor);
-	input.value = "";
+
+	const tarefa = document.createElement('li');
+	tarefa.classList.add('task');
+	const conteudo = `<p class="content">${valor}</p>`;
+
+	tarefa.innerHTML = conteudo;
+
+	tarefa.appendChild(criaBotaoConclui());
+	lista.appendChild(tarefa);
+	input.value = '';
 };
 
-const novaTarefa = document.querySelector("[data-form-button]");
+const criaBotaoConclui = () => {
+	const botaoConclui = document.createElement('button');
 
-novaTarefa.addEventListener("click", criaTarefa);
+	botaoConclui.classList.add('check-button');
+	botaoConclui.innerText = 'Concluír';
+
+	botaoConclui.addEventListener('click', concluirTarefa);
+	return botaoConclui;
+};
+
+const concluirTarefa = (evento) => {
+	const botaoConclui = evento.target;
+};
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criaTarefa);
