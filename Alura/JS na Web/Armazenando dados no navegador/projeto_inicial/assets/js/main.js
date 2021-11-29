c6cd8662e8122648062081bcaba772743d20c234
Felipe_SN <felipewfk@gmail.com>
2021-11-29 16:13:30 -0300

    final aula 1

--- Alura/JS na Web/Armazenando dados no navegador/projeto_inicial/assets/js/main.js
+++ Alura/JS na Web/Armazenando dados no navegador/projeto_inicial/assets/js/main.js
@@ -1,25 +1,39 @@
 import criaBotaoConclui from './components/BotaoConclui.js';
 import criaBotaoDeleta from './components/BotaoDeleta.js';
 
-const criaTarefa = (evento) => {
+const handleNovoItem = (evento) => {
 	evento.preventDefault();
 
 	const lista = document.querySelector('[data-list]');
 	const input = document.querySelector('[data-form-input]');
 	const valor = input.value;
 
+	const calendario = document.querySelector('[data-form-date]');
+	const data = moment(calendario.value);
+
+	const dataFormatada = data.format('DD/MM/YYYY');
+
+	const dados = { valor, dataFormatada };
+
+	const tarefa = criaTarefa(dados);
+
+	lista.appendChild(tarefa);
+	input.value = '';
+};
+
+const criaTarefa = ({ valor, dataFormatada }) => {
 	const tarefa = document.createElement('li');
 	tarefa.classList.add('task');
-	const conteudo = `<p class="content">${valor}</p>`;
+	const conteudo = `<p class="content">${dataFormatada} - ${valor}</p>`;
 
 	tarefa.innerHTML = conteudo;
 
 	tarefa.appendChild(criaBotaoConclui());
 	tarefa.appendChild(criaBotaoDeleta());
-	lista.appendChild(tarefa);
-	input.value = '';
+
+	return tarefa;
 };
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener('click', criaTarefa);
+novaTarefa.addEventListener('click', handleNovoItem);
