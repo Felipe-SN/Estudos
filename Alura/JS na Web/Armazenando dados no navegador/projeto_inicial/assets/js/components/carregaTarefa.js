6449b2ce82b48d5e418a1ad4047563cb41f902ae
Felipe_SN <felipewfk@gmail.com>
2021-11-30 14:56:30 -0300

    final aula 4

--- Alura/JS na Web/Armazenando dados no navegador/projeto_inicial/assets/js/components/carregaTarefa.js
+++ Alura/JS na Web/Armazenando dados no navegador/projeto_inicial/assets/js/components/carregaTarefa.js
@@ -1,11 +1,14 @@
-import { Tarefa } from './criaTarefa.js';
+import { ordenaDatas, removeDatasRepetidas } from '../services/data.js';
+import { criaData } from './criaData.js';
 
 export const carregaTarefa = () => {
 	const lista = document.querySelector('[data-list]');
 	const tarefaCadastrada = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 	lista.innerHTML = '';
-	tarefaCadastrada.forEach((tarefa) => {
-		lista.appendChild(Tarefa(tarefa));
+	const datasUnicas = removeDatasRepetidas(tarefaCadastrada);
+	ordenaDatas(datasUnicas);
+	datasUnicas.forEach((tarefa) => {
+		lista.appendChild(criaData(tarefa));
 	});
 };
