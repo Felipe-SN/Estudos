fadee0a1d0b79b357d6aaee8ee057c80499a8180
Felipe_SN <felipewfk@gmail.com>
2022-01-10 10:32:52 -0300

    var valorConvertido para var valoresConvertidos

--- Alura/Expressões Regulares/Exercicios/js/regex.js
+++ Alura/Expressões Regulares/Exercicios/js/regex.js
@@ -1,15 +1,36 @@
 var alvo = '11a22b33c';
-var exp = new RegExp('(\d\d)(\w)', 'g');
+// var exp = new RegExp('(\\d\\d)(\\w)', 'g');
+var exp = /(\d\d)(\w)/g;
+// var resultado = exp.exec(alvo);
+var resultado = '';
+// console.log(resultado);
+// console.log(exp.lastIndex);
 
-var resultado = exp.exec(alvo);
+while ((resultado = exp.exec(alvo))) {
+	if (resultado[0] === '') {
+		throw Error('Regex retornou valor vazio.');
+	}
 
-// while (resultado = exp.exec(textoTarget)) {
+	console.log(`Resultado: ${resultado} Posição: ${exp.lastIndex}`);
 
-// 	if(resultado[0] === "") {
-// 		throw Error("Regex retornou valor vazio.");
-// 	}
+	// resultados.push(geraResultado(mostraGrupos ? resultado.join(' ||| ') : resultado[0], resultado.index, objetoRegex.lastIndex, mostraIndex));
+}
 
-// 	console.log("Resultado: " + resultado[0]);
+//Alterando formato de data
 
-// 	resultados.push(geraResultado(mostraGrupos ? resultado.join(' ||| ') : resultado[0], resultado.index, objetoRegex.lastIndex, mostraIndex));
-// }
+var data = '01-01-2022';
+var exp = /-/g;
+data = data.replace(exp, '/');
+console.log(`Valor Alterado: ${data} Posição: ${exp.lastIndex}`);
+
+//Utilizando 'split'
+
+var arquivo = '100,200-150,200;20';
+var exp = /[,;-]/g;
+var valores = arquivo.split(exp);
+var valoresConvertidos = [];
+for (let i = 0; i < valores.length; i++) {
+	var int = parseFloat(valores[i]);
+	valoresConvertidos.push(int);
+}
+console.log(valoresConvertidos);
