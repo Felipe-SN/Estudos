ae4b533091e82412088465f50ae148b9de3ef401
Felipe_SN <felipewfk@gmail.com>
2022-01-11 17:31:43 -0300

    Aula 2.3

--- Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/admin/service/cliente-service.js
+++ Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/admin/service/cliente-service.js
@@ -19,21 +19,26 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]');
 
 const listaClientes = () => {
-	const promise = new Promise((resolve, reject) => {
-		const http = new XMLHttpRequest();
-		http.open('GET', 'http://localhost:3000/profile');
-
-		http.send();
-
-		http.onload = () => {
-			if (http.status >= 400) {
-				reject(JSON.parse(http.response));
-			} else {
-				resolve(JSON.parse(http.response));
-			}
-		};
+	return fetch(`http://localhost:3000/profile`).then((resposta) => {
+		return resposta.json();
 	});
-	return promise;
+
+	// -------------------------------------------------
+	// const promise = new Promise((resolve, reject) => {
+	// 	const http = new XMLHttpRequest();
+	// 	http.open('GET', 'http://localhost:3000/profile');
+
+	// 	http.send();
+
+	// 	http.onload = () => {
+	// 		if (http.status >= 400) {
+	// 			reject(JSON.parse(http.response));
+	// 		} else {
+	// 			resolve(JSON.parse(http.response));
+	// 		}
+	// 	};
+	// });
+	// return promise;
 };
 
 listaClientes().then((data) => {
