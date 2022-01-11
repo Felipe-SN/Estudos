1bba41f0ed4966edd39b85082913d4c22d721f0b
Felipe_SN <felipewfk@gmail.com>
2022-01-11 16:29:35 -0300

    final aula 1

--- Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/admin/service/cliente-service.js
+++ Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/admin/service/cliente-service.js
@@ -18,15 +18,26 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela]');
 
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+	const promise = new Promise((resolve, reject) => {
+		const http = new XMLHttpRequest();
+		http.open('GET', 'http://localhost:3000/profile');
 
-http.open('GET', 'http://localhost:3000/profile');
+		http.send();
 
-http.send();
+		http.onload = () => {
+			if (http.status >= 400) {
+				reject(JSON.parse(http.response));
+			} else {
+				resolve(JSON.parse(http.response));
+			}
+		};
+	});
+	return promise;
+};
 
-http.onload = () => {
-	const data = JSON.parse(http.response);
+listaClientes().then((data) => {
 	data.forEach((elemento) => {
 		tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
 	});
-};
+});
