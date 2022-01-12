fcbfa2d871e0773f4dc10475df1f37ad22df14c3
Felipe_SN <felipewfk@gmail.com>
2022-01-11 21:53:53 -0300

    Fix e mudanças proprias

--- Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/exercios_extras/automatizar_preenchimento/js/fetch.js
+++ Alura/JS na Web/CRUD com JavaScript assíncrono/projeto_inicial/exercios_extras/automatizar_preenchimento/js/fetch.js
@@ -1,34 +1,46 @@
+const inputDoCep = document.querySelector('#cep');
+const rua = document.querySelector('#rua');
+const complemento = document.querySelector('#complemento');
+const bairro = document.querySelector('#bairro');
+const cidade = document.querySelector('#cidade');
+const estado = document.querySelector('#estado');
+
 const btnPesquisarCEP = document.querySelector('#btnPesquisar');
 btnPesquisarCEP.addEventListener('click', (event) => {
-	event.PreventDefault();
+	event.preventDefault();
+
+	const valorDoCep = inputDoCep.value;
+	const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
+
+	fetch(url)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data.erro) {
+				alert('O CEP DIGITADO ESTÁ INVÁLIDO');
+				return;
+			}
+			atribuiCampos(data);
+		});
 });
 
-const inputDoCep = document.querySelector('#cep');
-const valorDoCep = inputDoCep.value;
-const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
+const btnLimpar = document.querySelector('#btnLimpar');
+btnLimpar.addEventListener('click', (event) => {
+	event.preventDefault();
 
-function atribuiCampos(data) {
-	const rua = document.querySelector('#rua');
-	const complemento = document.querySelector('#complemento');
-	const bairro = document.querySelector('#bairro');
-	const cidade = document.querySelector('#cidade');
-	const estado = document.querySelector('#estado');
+	inputDoCep.value = null;
+	rua.value = null;
+	complemento.value = null;
+	bairro.value = null;
+	cidade.value = null;
+	estado.value = null;
+});
 
+const atribuiCampos = (data) => {
 	rua.value = data.logradouro;
 	complemento.value = data.complemento;
 	bairro.value = data.bairro;
 	cidade.value = data.localidade;
 	estado.value = data.uf;
-}
-
-fetch(url)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		if (data.erro) {
-			alert('O CEP DIGITADO ESTÁ INVÁLIDO');
-			return;
-		}
-		atribuirCampos(data);
-	});
+};
