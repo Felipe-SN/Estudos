a66c49fca757897fc48e35c7c67f3ecc18013ba4
Felipe_SN <felipewfk@gmail.com>
2022-01-28 12:25:52 -0300

    final alula 3

--- Alura/JS na Web/Validação de Formularios e HTML5/validacao-doguito-main/assets/js/validacao.js
+++ Alura/JS na Web/Validação de Formularios e HTML5/validacao-doguito-main/assets/js/validacao.js
@@ -39,10 +39,15 @@ const mensagensDeErro = {
 		valueMissing: 'O campo data de nascimento não pode estar vazio.',
 		customError: 'Você deve ser maior de 18 anos para se cadastrar.',
 	},
+	cpf: {
+		valueMissing: 'O campo CPF não pode estar vazio.',
+		customError: 'O CPF digitado não é valido.',
+	},
 };
 
 const validadores = {
 	dataNascimento: (input) => validaDataNascimento(input),
+	cpf: (input) => validaCPF(input),
 };
 
 const mostraMensagemDeErro = (tipoDeInput, input) => {
@@ -75,4 +80,71 @@ const maiorQue18 = (data) => {
 	);
 
 	return dataMais18 <= dataAtual;
+};
+
+const validaCPF = (input) => {
+	const cpfFormatado = input.value.replace(/\D/g, '');
+	let mensagem = '';
+
+	if (!verificaCPFRepetido(cpfFormatado) || !validaEstruturaCPF(cpfFormatado)) {
+		mensagem = 'O CPF digitado não é valido.';
+	}
+
+	input.setCustomValidity(mensagem);
+};
+
+const verificaCPFRepetido = (cpf) => {
+	const valoresRepetidos = [
+		'00000000000',
+		'11111111111',
+		'22222222222',
+		'33333333333',
+		'44444444444',
+		'55555555555',
+		'66666666666',
+		'77777777777',
+		'88888888888',
+		'99999999999',
+	];
+
+	let cpfValido = true;
+
+	valoresRepetidos.forEach((valor) => {
+		if (valor == cpf) {
+			cpfValido = false;
+		}
+	});
+
+	return cpfValido;
+};
+
+const validaEstruturaCPF = (cpf) => {
+	const multiplicador = 10;
+
+	return validaDigitoVerificador(cpf, multiplicador);
+};
+
+const validaDigitoVerificador = (cpf, multiplicador) => {
+	if (multiplicador >= 12) {
+		return true;
+	}
+
+	let multiplicadorInicial = multiplicador;
+	let soma = 0;
+	const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+	const digitoVerificador = cpf.charAt(multiplicador - 1);
+	for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+		soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+		contador++;
+	}
+
+	if (digitoVerificador == confirmaDigito(soma)) {
+		return validaDigitoVerificador(cpf, multiplicador + 1);
+	}
+
+	return false;
+};
+
+const confirmaDigito = (soma) => {
+	return 11 - (soma % 11);
 };
