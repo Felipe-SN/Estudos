a7710b7c3260b4f8f18020345b17db69096e16a9
Felipe_SN <felipewfk@gmail.com>
2022-02-18 15:22:47 -0300

    Final do curso

--- Alura/JS na Web/Crie páginas dinâmicas/aluramidi/assets/js/main.js
+++ Alura/JS na Web/Crie páginas dinâmicas/aluramidi/assets/js/main.js
@@ -2,7 +2,14 @@ const listaTeclas = document.querySelectorAll('[data-tecla]');
 const listaSons = document.querySelectorAll('[data-som]');
 
 const tocaSom = (idAudio) => {
-  document.querySelector(idAudio).play();
+  const elemento = document.querySelector(idAudio);
+  const tagValida = elemento.localName === 'audio';
+
+  if (elemento && tagValida) {
+    elemento.play();
+    return;
+  }
+  console.log('Elemento não encontrado ou seletor inválido!');
 };
 
 for (let contador = 0; contador < listaTeclas.length; contador++) {
@@ -13,13 +20,19 @@ for (let contador = 0; contador < listaTeclas.length; contador++) {
   tecla.onclick = () => {
     tocaSom(idAudio);
   };
-}
 
-/*
-listaTeclas.forEach((tecla) => {
-  tecla.addEventListener('click', (e) => {
-    const tipoSom = e.target.dataset.tecla;
-    listaSons[tipoSom].play();
-  });
-});
-*/
+  tecla.onkeydown = (evento) => {
+    const code = evento.code;
+    const liberados = {
+      Space: 'Space',
+      Enter: 'Enter',
+    };
+    if (code === liberados[code]) {
+      tecla.classList.add('ativa');
+    }
+  };
+
+  tecla.onkeyup = () => {
+    tecla.classList.remove('ativa');
+  };
+}
