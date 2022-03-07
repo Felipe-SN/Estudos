2e0217b4df9b5d9f34ab70235f0d0ce1fd104635
Felipe_SN <felipewfk@gmail.com>
2022-03-07 18:01:02 -0300

    refac auto move

--- Alura/Jogos Clássicos/Parte 1/assets/js/main.js
+++ Alura/Jogos Clássicos/Parte 1/assets/js/main.js
@@ -4,28 +4,28 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.height = window.innerHeight - canvas.offsetHeight;
 ctx.canvas.width = window.innerWidth - canvas.offsetWidth;
 
-const lineSize = 10;
-const ballRadius = 10;
-const ballSpeed = 6;
-const bottomLineX = 10;
-const bottomLineY = canvas.height - lineSize * 2;
-const colorBg = 'black';
-const colorObjects = 'white';
-const player1X = lineSize;
-const player2X = canvas.width - lineSize * 2;
-const playerHeight = 100;
-const playersSpeed = 10;
-const topLineX = 10;
-const topLineY = 10;
-const xCenter = canvas.width / 2;
-const yCenter = canvas.height / 2;
+const lineSize = 10,
+  ballRadius = 10,
+  ballSpeed = 5,
+  bottomLineX = 10,
+  bottomLineY = canvas.height - lineSize * 2,
+  colorBg = 'black',
+  colorObjects = 'white',
+  player1X = lineSize,
+  player2X = canvas.width - lineSize * 2,
+  playerHeight = 100,
+  playersSpeed = 2 * ballSpeed,
+  topLineX = 10,
+  topLineY = 10,
+  xCenter = canvas.width / 2,
+  yCenter = canvas.height / 2;
 
-let player1Y = yCenter - playerHeight / 2;
-let player2Y = yCenter - playerHeight / 2;
-let xBall = xCenter;
-let xBallMove = ballSpeed;
-let yBall = yCenter;
-let yBallMove = ballSpeed;
+let player1Y = yCenter - playerHeight / 2,
+  player2Y = yCenter - playerHeight / 2,
+  xBall = xCenter,
+  xBallMove = ballSpeed,
+  yBall = yCenter,
+  yBallMove = ballSpeed;
 
 const drawBackground = () => {
   ctx.fillStyle = colorBg;
@@ -49,6 +49,20 @@ const drawFieldLines = () => {
   ctx.lineWidth = lineSize;
   ctx.strokeStyle = colorObjects;
   ctx.stroke();
+
+  /*centraliza e inverte o movimento da bola ao sair por uma das extremidades X do campo*/
+  if (xBall + ballRadius >= canvas.width || xBall - ballRadius <= 0) {
+    xBall = xCenter;
+    yBall = yCenter;
+    xBallMove = -xBallMove;
+  }
+  /*inverte a direção de movimento Y da bola ao tocar em uma das extremidades Y do campo*/
+  if (
+    yBall + ballRadius >= bottomLineY ||
+    yBall - ballRadius <= topLineY + lineSize
+  ) {
+    yBallMove = -yBallMove;
+  }
 };
 
 const drawBall = () => {
@@ -57,6 +71,10 @@ const drawBall = () => {
   ctx.beginPath();
   ctx.arc(xBall, yBall, ballRadius, 0, 2 * 3.14);
   ctx.fill();
+
+  /*adiciona o movimento a bola*/
+  xBall += xBallMove;
+  yBall += yBallMove;
 };
 
 const drawPlayers = () => {
@@ -67,49 +85,55 @@ const drawPlayers = () => {
   /*parâmetros referentes ao player2*/
   ctx.fillStyle = colorObjects;
   ctx.fillRect(player2X, player2Y, lineSize, playerHeight);
-};
-/*limpa atela para a apresentação de cada novo frame*/
-const clearCanvas = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-};
 
+  /*delimita os extremos dos players para ter interação com a bola*/
+  if (
+    (xBall + ballRadius >= player2X &&
+      yBall - ballRadius >= player2Y &&
+      yBall + ballRadius <= player2Y + playerHeight) ||
+    (xBall - ballRadius <= player1X + lineSize &&
+      yBall - ballRadius >= player1Y &&
+      yBall + ballRadius <= player1Y + playerHeight)
+  ) {
+    xBallMove = -xBallMove;
+  }
+};
+/*função para automatizar os movimentos do player2*/
 const autoMovementPlayer2 = () => {
-  const playerCenter = playerHeight / 2;
-  const playerTop = player2Y;
-  const playerBottom = player2Y + playerHeight;
-  const player2Speed = yBall - playerHeight / 2;
-  const hitTop =
-    playerTop <= topLineY + lineSize * 2 &&
-    yBall <= playerCenter + playerCenter / 2;
-  const hitBottom =
-    playerBottom >= bottomLineY - lineSize &&
-    yBall >= playerTop + playerCenter / 2;
+  const player2DetectionRange = xCenter + xCenter / 5,
+    player2Center = player2Y + playerHeight / 2;
 
-  if (xBall >= xCenter) {
-    if (hitTop || hitBottom) {
-      player2Y = player2Y;
-      return;
+  if (xBall >= player2DetectionRange) {
+    if (player2Y > topLineY + lineSize * 2 && yBall <= player2Center) {
+      player2Y -= playersSpeed;
     }
 
-    player2Y = player2Speed;
+    if (
+      player2Y + playerHeight < bottomLineY - lineSize &&
+      yBall >= player2Center
+    ) {
+      player2Y += playersSpeed;
+    }
   }
 };
 
+/*cuida das teclas pressionadas*/
 const handleKeysPressed = (event) => {
   const code = event.code;
   initializeCommand(event, code);
 };
 
+/*inicializa os comandos com base nas teclas pressionadas*/
 const initializeCommand = (event, code) => {
   const acceptedKeys = {
     /*comandos responsáveis pelo player2*/
     ArrowUp() {
-      if (player2Y > topLineY + lineSize) {
+      if (player2Y > topLineY + lineSize * 2) {
         player2Y -= playersSpeed;
       }
     },
     ArrowDown() {
-      if (player2Y + playerHeight < bottomLineY) {
+      if (player2Y + playerHeight < bottomLineY - lineSize) {
         player2Y += playersSpeed;
       }
     },
@@ -134,45 +158,15 @@ const initializeCommand = (event, code) => {
   }
 };
 
+/*adiciona um escutador para as teclas pressionadas*/
 window.addEventListener('keydown', handleKeysPressed);
 
-const ballInteractions = () => {
-  /*adiciona o movimento a bola*/
-  xBall += xBallMove;
-  yBall += yBallMove;
-
-  /*delimita os extremos dos players para ter interação com a bola*/
-  if (
-    (xBall + ballRadius >= player2X &&
-      yBall - ballRadius >= player2Y &&
-      yBall + ballRadius <= player2Y + playerHeight) ||
-    (xBall - ballRadius <= player1X + lineSize &&
-      yBall - ballRadius >= player1Y &&
-      yBall + ballRadius <= player1Y + playerHeight)
-  ) {
-    xBallMove = -xBallMove;
-  }
-  /*centraliza e inverte o movimento da bola ao sair por uma das extremidades X do campo*/
-  if (xBall + ballRadius >= canvas.width || xBall - ballRadius <= 0) {
-    xBall = xCenter;
-    yBall = yCenter;
-    xBallMove = -xBallMove;
-  }
-  /*inverte a direção de movimento Y da bola ao tocar em uma das extremidades Y do campo*/
-  if (
-    yBall + ballRadius >= bottomLineY ||
-    yBall - ballRadius <= topLineY + lineSize
-  ) {
-    yBallMove = -yBallMove;
-  }
-};
+/*faz a animação do jogo ocorrer*/
 const animation = () => {
-  clearCanvas();
   drawBackground();
   drawFieldLines();
   autoMovementPlayer2();
   drawBall();
-  ballInteractions();
   drawPlayers();
 
   requestAnimationFrame(animation);
