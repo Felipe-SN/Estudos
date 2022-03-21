72c96f47d37828b95ad26876acc4e2a4924c7633
Felipe_SN <felipewfk@gmail.com>
2022-03-21 10:46:09 -0300

    aula 3.7

--- Alura/React/Entendendo como a biblioteca funciona/ceep/src/components/LIstaDeNotas/ListaDeNotas.jsx
+++ Alura/React/Entendendo como a biblioteca funciona/ceep/src/components/LIstaDeNotas/ListaDeNotas.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import CardNota from './CardNota/CardNota';
+import CardNota from '../CardNota/';
+import './style.css';
 
 class ListaDeNotas extends Component {
   render() {
     return (
-      <ul>
-        {Array.of('Trabalho', 'Estudos').map((categoria, index) => {
+      <ul className="lista-notas">
+        {Array.of('Trabalho', 'Trabalho', 'Estudos').map((categoria, index) => {
           return (
-            <li key={index}>
-              <h2>{categoria}</h2>
+            <li className="lista-notas_item" key={index}>
               <CardNota />
             </li>
           );
