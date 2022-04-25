d6e89d06b11f838c355cb6a808211d5f5774f4b8
Felipe_SN <felipewfk@gmail.com>
2022-04-25 17:53:25 -0300

    meio-aula-7.6

--- Alura/React/Fundamentos de React/Escrevendo com Typescript/alura_studies/src/components/Cronometro/index.tsx
+++ Alura/React/Fundamentos de React/Escrevendo com Typescript/alura_studies/src/components/Cronometro/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Botao from '../Botao';
 import ITarefa from '../../types/tarefa';
 import Relogio from './Relogio';
@@ -10,14 +10,17 @@ interface Props {
 }
 
 export default function Cronometro({ selecionado }: Props) {
-  const [tempo, setTempo] = useState<Number>();
-  if (selecionado?.tempo) setTempo(tempoParaSegundos(selecionado.tempo));
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    selecionado?.tempo && setTempo(tempoParaSegundos(selecionado.tempo));
+  }, [selecionado]);
 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo} />
       </div>
       <Botao>Começar</Botao>
     </div>
