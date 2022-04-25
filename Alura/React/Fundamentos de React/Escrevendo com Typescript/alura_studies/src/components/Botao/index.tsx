faf62560d270716d62f2326f0d420bb2e693ba39
Felipe_SN <felipewfk@gmail.com>
2022-04-25 12:58:46 -0300

    aula-6

--- Alura/React/Fundamentos de React/Escrevendo com Typescript/alura_studies/src/components/Botao/index.tsx
+++ Alura/React/Fundamentos de React/Escrevendo com Typescript/alura_studies/src/components/Botao/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-type child = { children: string };
-
-class Botao extends React.Component<child> {
+class Botao extends React.Component<{
+  children: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+}> {
   render() {
-    console.log(this.props);
-
-    return <button className={style.botao}>{this.props.children}</button>;
+    const { type = 'button' } = this.props;
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
---
+++ Alura/React/Fundamentos de React/Escrevendo com Typescript/alura_studies/src/components/Lista/Item/index.tsx
@@ -0,0 +1,19 @@
+import ITarefa from '../../../types/tarefa';
+import style from '../Lista.module.scss';
+
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+}: ITarefa) {
+  console.log({ tarefa, tempo, selecionado, completado, id });
+
+  return (
+    <li className={style.item}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  );
+}
