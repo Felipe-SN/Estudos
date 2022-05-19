d6636c50553304eae9ed87248b3d205596d9dcf3
Felipe-SN <felipewfk@gmail.com>
2022-05-19 10:33:38 -0300

    aula6

--- Alura/React/Lidando com arquivos estáticos/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ Alura/React/Lidando com arquivos estáticos/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,16 +1,15 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg';
 import cardapio from '../itens.json';
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
-  const { title, description, photo, size, serving, price, id, category } =
-    props;
+  const { title, description, photo, size, serving, price, category } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt={title} />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
@@ -18,8 +17,15 @@ export default function Item(props: Props) {
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>{category.label}</div>
-          <div className={styles.item__porcao}>{size}</div>
+          <div
+            className={classNames(
+              styles.item__tipo,
+              styles[`item__tipo__${category.label.toLowerCase()}`]
+            )}
+          >
+            {category.label}
+          </div>
+          <div className={styles.item__porcao}>{size}g</div>
           <div className={styles.item__qtdpessoas}>
             Serve {serving} pessoa{serving === 1 ? '' : 's'}
           </div>
