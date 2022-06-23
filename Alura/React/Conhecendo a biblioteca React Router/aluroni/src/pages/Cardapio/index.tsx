7e2a1df5d9326354bb6be0cfb7eb1b5aee84a5fa
Felipe-SN <felipewfk@gmail.com>
2022-06-23 16:03:59 -0300

    aula4.6

--- Alura/React/Conhecendo a biblioteca React Router/aluroni/src/pages/Cardapio/index.tsx
+++ Alura/React/Conhecendo a biblioteca React Router/aluroni/src/pages/Cardapio/index.tsx
@@ -1,7 +1,7 @@
-import { ReactComponent as Logo } from 'assets/logo.svg';
 import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
+import theme from 'styles/tema.module.scss';
 import Filtros from './Filtros';
 import Ordenador, { OpcoesOrdenador } from './Ordenador';
 import Itens from './Itens';
@@ -11,22 +11,15 @@ export default function Cardapio() {
   const [filtro, setFiltro] = useState<number | null>(null);
   const [ordenador, setOrdenador] = useState<OpcoesOrdenador>('');
   return (
-    <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
-      <header className={styles.header}>
-        <div className={styles.header__text}>A casa do código e da massa</div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca} />
-        <div className={styles.cardapio__filtros}>
-          <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-        </div>
-        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-      </section>
-    </main>
+    <section className={styles.cardapio}>
+      <h3 className={theme.titulo}>Cardápio</h3>
+      <Buscador busca={busca} setBusca={setBusca} />
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+    </section>
   );
 }
+
