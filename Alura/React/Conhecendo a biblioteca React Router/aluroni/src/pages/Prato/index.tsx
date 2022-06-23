51611458f8d56da70ad39bd0283afc6e26d84cdf
Felipe-SN <felipewfk@gmail.com>
2022-06-23 17:53:17 -0300

    Aula5

---
+++ Alura/React/Conhecendo a biblioteca React Router/aluroni/src/pages/Prato/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Prato.module.scss';
+import { useLocation } from 'react-router-dom';
+
+export default function Prato() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
