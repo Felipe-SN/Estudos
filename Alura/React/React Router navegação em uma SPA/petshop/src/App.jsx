8cfaebd5e76ae7323ae9d3f6686698c0a4797b61
Felipe-SN <felipewfk@gmail.com>
2022-07-18 17:58:42 -0300

    aula2

--- Alura/React/React Router navegação em uma SPA/petshop/src/App.jsx
+++ Alura/React/React Router navegação em uma SPA/petshop/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import 'assets/css/base/base.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Home from 'pages/Home';
+import Sobre from 'pages/Sobre';
+import Error404 from 'pages/Error404';
 
 function App() {
   return (
-  <>
-
-  </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
