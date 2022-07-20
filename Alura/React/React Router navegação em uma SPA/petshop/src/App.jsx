a4da634430096d9e012a8242649559d0e08a7130
Felipe-SN <felipewfk@gmail.com>
2022-07-20 15:05:41 -0300

    add component header

--- Alura/React/React Router navegação em uma SPA/petshop/src/App.jsx
+++ Alura/React/React Router navegação em uma SPA/petshop/src/App.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 import Home from 'pages/Home';
 import Sobre from 'pages/Sobre';
 import Error404 from 'pages/Error404';
+import Header from 'components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
