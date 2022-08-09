58d865f1671ea97ecf12ff715563c6bd75568e53
Felipe-SN <felipewfk@gmail.com>
2022-08-09 16:46:05 -0300

    criação do mock de dados

--- Alura/React/Gerenciando estado com Recoil/event-tracker/src/state/atom.ts
+++ Alura/React/Gerenciando estado com Recoil/event-tracker/src/state/atom.ts
@@ -4,22 +4,7 @@ import { atom } from 'recoil';
 
 export const listaEventosState = atom<IEvento[]>({
   key: 'listaEventosState',
-  default: [
-    {
-      descricao: 'Estudar React',
-      inicio: new Date('2022-08-11T09:00'),
-      fim: new Date('2022-08-11T13:00'),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: 'Estudar Recoil',
-      inicio: new Date('2022-08-12T09:00'),
-      fim: new Date('2022-08-12T11:00'),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: [],
 });
 
 export const filtroEventos = atom<IFiltroEventos>({
