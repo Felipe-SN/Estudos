d626f1cd4a7292595bf913eda248ac04b0769d01
Felipe-SN <felipewfk@gmail.com>
2022-08-10 16:49:59 -0300

    limpeza do projeto

--- Alura/React/Testando os seus componentes/sorteador-de-amigo-secreto-main/src/App.tsx
+++ Alura/React/Testando os seus componentes/sorteador-de-amigo-secreto-main/src/App.tsx
@@ -1,13 +1,17 @@
 import Formulario from 'components/Formulario';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const App = () => {
   return (
-    <div className="App">
-      <Formulario />
-    </div>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<Formulario />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 };
 
 export default App;
-
