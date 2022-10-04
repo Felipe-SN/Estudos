56e84c92de9166b4994e48ccaa1176825cd7f558
Felipe-SN <felipewfk@gmail.com>
2022-10-04 11:44:29 -0300

    aula 6 As vantagens e utilização de grid areas construção da pagina picos

--- Alura/CSS/Dispondo elementos com Flexbox e Grid/flex-e-grid-main/js/index.js
+++ Alura/CSS/Dispondo elementos com Flexbox e Grid/flex-e-grid-main/js/index.js
@@ -1,16 +1,23 @@
+let paginaAtual = window.location.pathname.replace(/.*\/|\..*/g, '');
 const btnMenu = document.querySelector('.cabecalho__menu');
 const menuLateral = document.querySelector('.menu-lateral');
 const menuLateralLinks = document.querySelectorAll('.menu-lateral__link');
+const pages = {
+  index: '0',
+  videos: '1',
+  picos: '2',
+  integrantes: '3',
+  camisas: '4',
+  pinturas: '5',
+};
 
 btnMenu.addEventListener('click', () => {
   menuLateral.classList.toggle('menu-lateral--ativo');
 });
 
-menuLateralLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    link.classList.add('menu-lateral__link--ativo');
-  });
-  link.addEventListener('blur', () =>
-    link.classList.remove('menu-lateral__link--ativo')
-  );
-});
+if (pages[paginaAtual])
+  (() => {
+    menuLateralLinks[pages[paginaAtual]].classList.toggle(
+      'menu-lateral__link--ativo'
+    );
+  })();
