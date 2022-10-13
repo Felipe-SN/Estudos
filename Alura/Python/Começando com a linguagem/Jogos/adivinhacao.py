10aa87252fb5dd3d30b6f268e354773e93290644
Felipe-SN <felipewfk@gmail.com>
2022-10-13 11:16:48 -0300

    aula 4

--- Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
+++ Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
@@ -6,22 +6,31 @@ print('Bem-vindo ao Jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute_str = input('Digite o seu numero: ')
+for rodada in range(1, total_de_tentativas + 1):
+    print('Tentativas {} de {}'.format(rodada, total_de_tentativas))
 
-print('Você Digitou: ', chute_str)
+    chute_str = input('Digite um número entre 0 e 100: ')
+    chute_int = int(chute_str)
 
-chute_int = int(chute_str)
+    if (chute_int < 1 or chute_int > 100):
+        print('Você deve digitar um número entre 0 e 100')
+        continue
 
-acertou = chute_int == numero_secreto
-chute_foi_maior = chute_int > numero_secreto
-chute_foi_menor = chute_int < numero_secreto
+    print('Você Digitou: {}'.format(chute_int))
 
-if (acertou):
-    print('Você Acertou,', chute_int, 'é o Numero Correto!')
-elif (chute_foi_maior):
-    print('Você Errou,', chute_int, 'é maior que o Numero Correto!')
-elif (chute_foi_menor):
-    print('Você Errou,', chute_int, 'é menor que o Numero Correto!')
+    acertou = chute_int == numero_secreto
+    chute_foi_maior = chute_int > numero_secreto
+    chute_foi_menor = chute_int < numero_secreto
+
+    if (acertou):
+        print('Você Acertou!')
+        break
+    else:
+        if (chute_foi_maior):
+            print('Você Errou, o numero correto é menor!')
+        elif (chute_foi_menor):
+            print('Você Errou, o numero correto é maior!')
 
 print('Fim de Jogo!')
