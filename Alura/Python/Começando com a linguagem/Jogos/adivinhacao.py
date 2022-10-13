e57503574a8aea494caa09fc73b59fd8d93e8f6c
Felipe-SN <felipewfk@gmail.com>
2022-10-13 12:07:25 -0300

    aula 6

--- Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
+++ Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
@@ -1,36 +1,39 @@
-from turtle import goto
-
+import random
 
 print('*********************************')
-print('Bem-vindo ao Jogo de Adivinhação!')
+print('*********************************', end='\n\n')
+print('Bem-vindo ao Jogo de Adivinhação!', end='\n\n')
 print('*********************************')
+print('*********************************', end='\n\n')
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print(numero_secreto, end='\n\n')
+
 for rodada in range(1, total_de_tentativas + 1):
-    print('Tentativas {} de {}'.format(rodada, total_de_tentativas))
+    print('Tentativas {} de {}'.format(rodada, total_de_tentativas), end='\n\n')
 
     chute_str = input('Digite um número entre 0 e 100: ')
     chute_int = int(chute_str)
 
     if (chute_int < 1 or chute_int > 100):
-        print('Você deve digitar um número entre 0 e 100')
+        print('Você deve digitar um número entre 0 e 100', end='\n\n')
         continue
 
-    print('Você Digitou: {}'.format(chute_int))
+    print('Você Digitou: {}'.format(chute_int), end='\n\n')
 
     acertou = chute_int == numero_secreto
     chute_foi_maior = chute_int > numero_secreto
     chute_foi_menor = chute_int < numero_secreto
 
     if (acertou):
-        print('Você Acertou!')
+        print('Você Acertou!', end='\n\n')
         break
     else:
         if (chute_foi_maior):
-            print('Você Errou, o numero correto é menor!')
+            print('Você Errou, o numero correto é menor!', end='\n\n')
         elif (chute_foi_menor):
-            print('Você Errou, o numero correto é maior!')
+            print('Você Errou, o numero correto é maior!', end='\n\n')
 
 print('Fim de Jogo!')
