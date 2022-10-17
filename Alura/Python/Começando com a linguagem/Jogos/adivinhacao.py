445c76a7542ab0e76df66fbd014ef5a90685868d
Felipe-SN <felipewfk@gmail.com>
2022-10-17 11:29:49 -0300

    aula 7

--- Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
+++ Alura/Python/Começando com a linguagem/Jogos/adivinhacao.py
@@ -7,33 +7,50 @@ print('*********************************')
 print('*********************************', end='\n\n')
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
 
-print(numero_secreto, end='\n\n')
+print('Qual nível de dificuldade?')
+print('(1) Fácil / (2) Médio / (3) Difícil')
+dificuldade = int(input('Defina a dificuldade: '))
+
+
+if (dificuldade == 1):
+    total_de_tentativas = 20
+elif (dificuldade == 2):
+    total_de_tentativas = 10
+elif (dificuldade == 3):
+    total_de_tentativas = 5
+else:
+    total_de_tentativas = 10
+    print('Dificuldade invalida, utilizando dificuldade padrão: Médio.')
+
+print('\n', numero_secreto, end='\n\n')
 
 for rodada in range(1, total_de_tentativas + 1):
     print('Tentativas {} de {}'.format(rodada, total_de_tentativas), end='\n\n')
 
-    chute_str = input('Digite um número entre 0 e 100: ')
-    chute_int = int(chute_str)
+    chute = int(input('Digite um número entre 0 e 100: '))
 
-    if (chute_int < 1 or chute_int > 100):
+    if (chute < 1 or chute > 100):
         print('Você deve digitar um número entre 0 e 100', end='\n\n')
         continue
 
-    print('Você Digitou: {}'.format(chute_int), end='\n\n')
+    print('Você Digitou: {}'.format(chute), end='\n\n')
 
-    acertou = chute_int == numero_secreto
-    chute_foi_maior = chute_int > numero_secreto
-    chute_foi_menor = chute_int < numero_secreto
+    acertou = chute == numero_secreto
+    chute_foi_maior = chute > numero_secreto
+    chute_foi_menor = chute < numero_secreto
 
     if (acertou):
-        print('Você Acertou!', end='\n\n')
+        print('Você Acertou! Sua pontuação é : {}'.format(pontos), end='\n\n')
         break
     else:
         if (chute_foi_maior):
             print('Você Errou, o numero correto é menor!', end='\n\n')
         elif (chute_foi_menor):
             print('Você Errou, o numero correto é maior!', end='\n\n')
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print('Fim de Jogo!')
