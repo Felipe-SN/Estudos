049eaa9e623b7fe06e3697822d20bcbeafa955e0
Felipe-SN <felipewfk@gmail.com>
2022-10-19 10:46:01 -0300

    aula 2

--- Alura/Python/Avançando na linguagem/Jogos/forca.py
+++ Alura/Python/Avançando na linguagem/Jogos/forca.py
@@ -4,15 +4,31 @@ def jogar():
     print('**** Bem-vindo ao Jogo da Forca! ****')
     print('*************************************')
 
-    palavra_secreta = 'berserk'
+    palavra_secreta = 'barbarian'
+
     enforcado = False
     acertou = False
+    total_de_tentativas = 5
+    rodada = 1
 
-    while (not enforcado and not acertou):
+    # while (not enforcado and not acertou):
+    while (rodada <= total_de_tentativas):
         print('\njogando...')
-        chute = input('\ndigite seu chute: ')
-        if (chute == palavra_secreta):
-            acertou = True
+
+        chute = input('\nDigite seu chute: ').strip()
+        index = 0
+        encontrou = False
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                encontrou = True
+                print('A letra {} foi encontrada na posição {}'.format(letra, index))
+            index = index + 1
+
+        if (not encontrou):
+            print('A letra digitada não foi encontrada na palavra.')
+
+        rodada = rodada + 1
 
     print('\nFim de Jogo!')
 
