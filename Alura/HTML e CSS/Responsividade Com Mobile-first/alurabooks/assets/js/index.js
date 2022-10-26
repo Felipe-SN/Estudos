56d7c0cfab02c71426d1684bd36ee3341749afab
Felipe-SN <felipewfk@gmail.com>
2022-10-26 12:47:11 -0300

    Aplicar plugins externos através de CDN Utilizar o plugin SwiperJS Reforçar o aprendizado de flexbox Reutilizar elementos e estilos Criar listas Editar input e seu placeholder

--- Alura/HTML e CSS/Responsividade Com Mobile-first/alurabooks/assets/js/index.js
+++ Alura/HTML e CSS/Responsividade Com Mobile-first/alurabooks/assets/js/index.js
@@ -5,3 +5,11 @@ btnMenu.addEventListener('click', () => {
   menu.classList.toggle('cabecalho__lista-menu--ativo');
   btnMenu.classList.toggle('cabecalho__menu-hamburger--ativo');
 });
+
+const swiper = new Swiper('.swiper', {
+  centeredSlides: true,
+  loop: true,
+  pagination: { el: '.swiper-pagination', type: 'bullets' },
+  slidesPerView: 3,
+  spaceBetween: 150,
+});
