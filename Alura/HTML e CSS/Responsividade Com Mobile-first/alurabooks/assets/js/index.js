ef036b5a500072c0003f1477cf3df95ac1edecb1
Felipe-SN <felipewfk@gmail.com>
2022-11-09 12:29:11 -0300

    Usar media queries Aplicar diferentes estilos para diferentes tamanhos de tela Retirar elementos da tela Aplicar elementos na tela de acordo com o tamanho do dispositivo

--- Alura/HTML e CSS/Responsividade Com Mobile-first/alurabooks/assets/js/index.js
+++ Alura/HTML e CSS/Responsividade Com Mobile-first/alurabooks/assets/js/index.js
@@ -1,15 +1,64 @@
-const btnMenu = document.querySelector('.cabecalho__menu-hamburger');
-const menu = document.querySelector('.cabecalho__lista-menu');
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-btnMenu.addEventListener('click', () => {
-  menu.classList.toggle('cabecalho__lista-menu--ativo');
-  btnMenu.classList.toggle('cabecalho__menu-hamburger--ativo');
-});
+const btnMenu = document.querySelector('.cabecalho__menu-hamburger');
+const btnCategorias = document.querySelector('[data-btn-categoria]');
+const menu = document.querySelector('.cabecalho__lista-menu');
+const swapVersion = {
+  true: () =>
+    ativaElementoDeClique(btnMenu, 'cabecalho__menu-hamburger--ativo'),
+  false: () =>
+    ativaElementoDeClique(btnCategorias, 'cabecalho__menu-hamburger--ativo'),
+};
+
+let larguraTela = window.innerWidth;
+let mobileVersion = (larguraTela < 1024).toString();
+
+const ativaElementoDeClique = (elementoAtivo, classeAtiva) => {
+  const controlador = new AbortController();
+  if (elementoAtivo) {
+    elementoAtivo.addEventListener(
+      'click',
+      () => {
+        menu.classList.toggle('cabecalho__lista-menu--ativo');
+        elementoAtivo.classList.toggle(classeAtiva);
+      },
+      {
+        signal: controlador.signal,
+      }
+    );
+  }
+  return controlador;
+};
+
+let elementoAtual = swapVersion[mobileVersion]();
+
+window.onresize = () => {
+  larguraTela = window.innerWidth;
+  mobileVersion = (larguraTela < 1024).toString();
+  elementoAtual.abort();
+  if (menu.classList.contains('cabecalho__lista-menu--ativo')) {
+    menu.classList.remove('cabecalho__lista-menu--ativo');
+    btnMenu.classList.remove('cabecalho__menu-hamburger--ativo');
+    btnCategorias.classList.remove('cabecalho__menu-hamburger--ativo');
+  }
+
+  if (!mobileVersion) {
+    elementoAtual = swapVersion[mobileVersion]();
+    return;
+  }
+  elementoAtual = swapVersion[mobileVersion]();
+  return;
+};
 
 const swiper = new Swiper('.swiper', {
   centeredSlides: true,
-  loop: true,
+  centeredSlidesBounds: true,
   pagination: { el: '.swiper-pagination', type: 'bullets' },
-  slidesPerView: 3,
-  spaceBetween: 150,
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  breakpoints: {
+    1024: {
+      spaceBetween: 0,
+    },
+  },
 });
