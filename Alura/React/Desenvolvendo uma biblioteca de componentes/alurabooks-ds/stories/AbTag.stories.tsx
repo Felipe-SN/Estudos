a3344b73add75437001734c049c6666996ff65d6
Felipe-SN <felipewfk@gmail.com>
2022-11-21 08:58:40 -0300

    evoluir um componente existente, atualizar uma story para contemplar suas variantes, criar um componente de input de texto, atualizar a versão do pacote NPM manualmente

---
+++ Alura/React/Desenvolvendo uma biblioteca de componentes/alurabooks-ds/stories/AbTag.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { AbTag } from '../src/components/AbTag';
+
+type TagType = typeof AbTag;
+
+export default {
+  title: 'components/AbTag',
+  component: AbTag,
+} as ComponentMeta<TagType>;
+
+const Template: ComponentStory<TagType> = args => <AbTag {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  text: 'Your Tag Name',
+};
