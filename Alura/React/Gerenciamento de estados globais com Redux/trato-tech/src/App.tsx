ba24e1b4813b0eeb4f7381e17c2900f5cec085b5
Felipe-SN <felipewfk@gmail.com>
2023-02-27 17:48:07 -0300

    tweaks na config do lint

--- Alura/React/Gerenciamento de estados globais com Redux/trato-tech/src/App.tsx
+++ Alura/React/Gerenciamento de estados globais com Redux/trato-tech/src/App.tsx
@@ -1,27 +1,8 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a rel="noopener" href="https://vitejs.dev" target="_blank">
-          A
-        </a>
-        <a rel="noopener" href="https://reactjs.org" target="_blank">
-          B
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
+    <>
+      <></>
+    </>
   );
 }
 
