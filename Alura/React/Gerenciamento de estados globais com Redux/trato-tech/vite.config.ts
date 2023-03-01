e0ffc57f239046dd86fc6e99661797c685b6fdc1
Felipe-SN <felipewfk@gmail.com>
2023-03-01 16:26:04 -0300

    inicio de criação do componente NavBar e Busca

--- Alura/React/Gerenciamento de estados globais com Redux/trato-tech/vite.config.ts
+++ Alura/React/Gerenciamento de estados globais com Redux/trato-tech/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import viteSvgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), tsconfigPaths(), viteSvgr()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/breakpoints";`,
+      },
+    },
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
 });
